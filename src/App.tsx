543f6f6e20fe3dc8b7a677846436106f3e04ec17
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2020-04-05 16:38:11 -0500

    setup module resolver

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+import Home from 'screens/home';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ src/screens/home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={styles.main}>Engine: Hermes</Text>
+    </View>
+  );
+};
+
+export default Home;
